97fceaf4c76e886005d15ff92fa4bcc5ea49373c
devarogundade <devarogundade@gmail.com>
2025-03-16 18:27:17 +0100

    update sdk endpoint

--- beam-sdk/src/utils/endpoints.ts
+++ beam-sdk/src/utils/endpoints.ts
@@ -3,6 +3,6 @@ export const Endpoints = {
     Testnet: "https://api.studio.thegraph.com/query/106337/beam/version/latest",
   },
   BASE_TRANSACTION_URL: {
-    Testnet: "http://localhost:5174",
+    Testnet: "https://beam-payment.netlify.app",
   },
 };
